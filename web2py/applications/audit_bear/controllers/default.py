d54180a2da6f61a90587f67649318c78ca88ff43
samus250 <samus250@7335eb47-6040-785b-8377-bf3fc35e9a06>
2011-06-23 23:17:11 +0000

    Added form on index file. Check comments on default.py controller for planned application workflow. Added results function and view.

--- web2py/applications/audit_bear/controllers/default.py
+++ web2py/applications/audit_bear/controllers/default.py
@@ -1,2 +1,18 @@
+# Model - runs before the controller, defines data structures, methods,
+# classes, etc. and passes global names to the controller.
+# Dispatcher class might be defined there
+
+# Controller (this file) - application workflow
+# uses models objects to run application workflow, passes dictionary
+# to view
+
+# index shows the homepage and form, accepts both 152 and 155 file
 def index():
-    return dict(message='Say hello to Audit Bear')
+    form = FORM('upload audit log:', INPUT(_name='audit_log', _type='file'),
+        INPUT(_name='ballot_image', _type='file'),
+        INPUT(_type='submit'), _action='results')
+    return dict(message='Say hello to Audit Bear', form=form)
+
+# all the results
+def results():
+    return dict(message='YOUR RESULTS: LOLOLOLOL')
