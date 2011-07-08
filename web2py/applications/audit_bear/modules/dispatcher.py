ea9269da4f5bc0b520554759857fe2f3a13323a5
ae243 <ae243@cornell.edu>
2011-07-08 01:05:57 +0000

    Updated dispatcher

--- web2py/applications/audit_bear/modules/dispatcher.py
+++ web2py/applications/audit_bear/modules/dispatcher.py
@@ -11,12 +11,28 @@ def dispatcher(el152=None, el155=None, el68a=None):
     results = []
 
     #Adding some basic analysis stuff
-    if el155 != None and el152 != None:
+    if el155 != None and el152 != None and el68a != None:
         #Adding some Init stuff we will all probably need
         data = auditLog.AuditLog(el152)
-        ballot = ballotImage.BallotImage(el155, data)
+
         parsedEL68 = el68aParser.EL68A(el68a)
+        ballot = ballotImage.BallotImage(el155, data, parsedEL68)
         dateclass = dateMod.DateMod(data, parsedEL68.electionDate)
+        
+        
+        #Start running analysis
+        results.append(myanalyses.dateanomalies(data, dateclass))
+        results.append(myanalyses.openmachines(dateclass))
+        results.append(eventAnomalies(data, report.Report()))
+        #results.append(lowBatteryMachines(data,ballot,report.Report()))
+        results.append(getWarningEvents(data,ballot,report.Report()))
+        results.append(getVoteCancelledEvents(data,ballot,report.Report()))
+        return dict(message='files recieved', results=results)
+        
+    elif el155 != None and el152 != None and el68a == None:
+        data = auditLog.AuditLog(el152)
+        ballot = ballotImage.BallotImage(el155, data)
+        dateclass = dateMod.DateMod(data, None)
 
         #Start running analysis
         results.append(myanalyses.dateanomalies(data, dateclass))
